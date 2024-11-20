3e5c9af27090c5700ecf6fe6ebf2d85f2bb1ddc1
Marcos Tirao <marcos.tirao@icloud.com>
2024-11-20 19:01:47 +0100

    Initial Commit

---
+++ Wanaka Tracker Manager/Wanaka_Tracker_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wanaka_Tracker_ManagerApp.swift
+//  Wanaka Tracker Manager
+//
+//  Created by Marcos Tirao on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Wanaka_Tracker_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wanaka Tracker ManagerTests/Wanaka_Tracker_ManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wanaka_Tracker_ManagerTests.swift
+//  Wanaka Tracker ManagerTests
+//
+//  Created by Marcos Tirao on 20/11/2024.
+//
+
+import Testing
+@testable import Wanaka_Tracker_Manager
+
+struct Wanaka_Tracker_ManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
